79d058204bf68fa083885905e6ecd885f04b58e8
bbing2 <rlans1947@naver.com>
2023-02-08 00:44:12 +0900

    호출 시 반환되는 값이 1씩 커지는 컨트롤러 액션 구현

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/home/controller/UsrHomeController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/home/controller/UsrHomeController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 
 public class UsrHomeController {
+  private int count;
+  public UsrHomeController() {
+    count = 0;
+  }
+
   @RequestMapping("/usr/home/main")
   @ResponseBody
   public String showMain(){
@@ -21,5 +26,12 @@ public class UsrHomeController {
   @ResponseBody
   public String showMain3(){
     return "또 만나요.";
+  }
+
+  @RequestMapping("/usr/home/main4")
+  @ResponseBody
+  public int showMain4(){
+    count++;
+    return count;
   }
 }
