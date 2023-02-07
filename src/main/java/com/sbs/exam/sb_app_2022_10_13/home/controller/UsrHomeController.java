388a343a66fa75f0dbaa0a1d6c5311dedc51d158
bbing2 <rlans1947@naver.com>
2023-02-08 00:53:34 +0900

    count 변수의 초기화 액션 구현

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/home/controller/UsrHomeController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/home/controller/UsrHomeController.java
@@ -8,30 +8,41 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 public class UsrHomeController {
   private int count;
+
   public UsrHomeController() {
     count = 0;
   }
 
   @RequestMapping("/usr/home/main")
   @ResponseBody
-  public String showMain(){
+  public String showMain() {
     return "안녕하세요.";
   }
+
   @RequestMapping("/usr/home/main2")
   @ResponseBody
-  public String showMain2(){
+  public String showMain2() {
     return "반갑습니다.";
   }
+
   @RequestMapping("/usr/home/main3")
   @ResponseBody
-  public String showMain3(){
+  public String showMain3() {
     return "또 만나요.";
   }
 
   @RequestMapping("/usr/home/main4")
   @ResponseBody
-  public int showMain4(){
+  public int showMain4() {
     count++;
     return count;
+  }
+
+  @RequestMapping("/usr/home/main5")
+  @ResponseBody
+  public String showMain5() {
+    count = 0;
+    return "count의 값이 0으로 초기화 되었습니다.";
+
   }
 }
